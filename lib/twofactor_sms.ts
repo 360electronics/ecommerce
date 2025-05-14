e0fcb2e00496f1aaef4c28fd225ed479c9f324ef
Manoj Kumar <manojkumararumainathan@gmail.com>
2025-05-15 00:59:35 +0530

    Update: OTP Verification SMS' 2Factor

--- lib/twofactor_sms.ts
+++ lib/twofactor_sms.ts
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 export async function sendSmsOTP(phoneNumber: string, otp: string) {
   const twoFactorApiKey = process.env.TWO_FACTOR_API_KEY;
-  const twoFactorTemplate = process.env.TWO_FACTOR_TEMPLATE_NAME || '';
 
   try {
     // This is specific to 2Factor's API format
-    const response = await axios.get(`https://2factor.in/API/V1/${twoFactorApiKey}/SMS/${phoneNumber}/${otp}/${twoFactorTemplate}`);
+    const response = await axios.get(`https://2factor.in/API/V1/${twoFactorApiKey}/SMS/${phoneNumber}/${otp}/REDELE`);
     
     if (response.data.Status === 'Success') {
       return true;
