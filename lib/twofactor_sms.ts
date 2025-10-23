753a7b4c149a3824b9e6fa8ccabc817c14959fa9
Manoj Kumar <manojkumararumainathan@gmail.com>
2025-10-23 21:52:59 +0530

    2factor otp

--- lib/twofactor_sms.ts
+++ lib/twofactor_sms.ts
@@ -1,19 +1,26 @@
-import axios from 'axios'
+import axios from "axios";
 
 export async function sendSmsOTP(phoneNumber: string, otp: string) {
   const twoFactorApiKey = process.env.TWO_FACTOR_API_KEY; // Your 2Factor API Key
 
+  if (!twoFactorApiKey) {
+    throw new Error("Missing TWO_FACTOR_API_KEY in environment variables");
+  }
+
   try {
-    const url = `https://2factor.in/API/R1/?module=TRANS_SMS&apikey=${twoFactorApiKey}&to=${phoneNumber}&from=GARAGE&templatename=360Electronics&var1=${otp}`;
+    const url = `https://2factor.in/API/R1/?module=TRANS_SMS&apikey=${twoFactorApiKey}&to=${phoneNumber}&from=GARAGE&templatename=360Electronics-Ecom&var1=${otp}`;
 
-    const response = await axios.get(url);
+    const { data } = await axios.get(url);
 
-    if (response.data.Status === 'Success') {
+    if (data.Status === "Success") {
+      // console.log("✅ OTP SMS sent successfully to", phoneNumber);
       return true;
+    } else {
+      console.error("❌ Failed to send OTP:", data);
+      return false;
     }
-    return false;
-  } catch (error) {
-    console.error('Error sending transactional SMS:', error);
+  } catch (error: any) {
+    console.error("🚨 Error sending transactional SMS:", error.message || error);
     return false;
   }
 }
